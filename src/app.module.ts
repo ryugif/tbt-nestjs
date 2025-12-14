3cc7f8291b0138ccbfb057005d6b81a855cfc2ae
Ramadhan Wahyu <ramadhanwahyu24@gmail.com>
2025-12-14 07:45:49 +0700

    feat: add rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ResponseInterceptor } from './interceptors/response.interceptor';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     ModulesModule,
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
   ],
   controllers: [AppController],
   providers: [
